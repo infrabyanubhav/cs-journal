b067c37859616e04c0f6b84937fcd56b14496538
Anubhav Sharma <infrabyanubhav@gmail.com>
2026-06-12 20:17:17 +0530

    Add ConversionOperator class for time conversion
    Implement a ConversionOperator class for time conversion.

---
+++ cpp/ConversionOperator.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Time{
+   
+    int hrs;
+    int min;
+    public:
+    Time(){};
+    Time operator=(int t){
+        cout<<"Conversion of basic to class type t"<<endl;
+        hrs=t/60;
+        min=t%60;
+        cout << hrs << " Hr : " << min << " min" << endl;
+        return *this;
+    }
+};
+
+int main(){
+    cout<<"Executing....."<<endl;
+    int ti=150;
+    Time t1;
+    t1= (ti);
+    return 0;
+}
